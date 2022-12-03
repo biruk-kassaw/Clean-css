5a1b05cc1cf9bac24ff6346b4d71e75634fd841b
biruk-kassaw <se.biruk.kassaw@gmail.com>
2022-12-03 10:44:00 +0300

    romoving psudo class selectors from selectors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {CssParser} = require("./parsers/CssParser");
 const {HtmlParser} = require("./parsers/HtmlParser");
-const {trimSelector} = require("./utils/TrimString");
+const {trimSelector, removePsudoSelectors} = require("./utils/TrimString");
+
 (async () => {
   const parsedHtml = await HtmlParser("./seo-agency-website-template/seo-agency-website-template/index.html")
   const parsedCSS = await CssParser("./seo-agency-website-template/seo-agency-website-template/css/sample.css")
@@ -13,19 +14,22 @@ const {trimSelector} = require("./utils/TrimString");
   extractedAtusedCss = []
   extractedusedCss = []
 
-
+  
   parsedCSS.forEach(element => {
-    // trim element.selector to remove /n
+    // trim element.selector to remove /n 
     let trimedSelector = trimSelector(element.selector);
 
+    // check if it is an at selector
     if(trimedSelector.startsWith("@")){
-
+      // check if it has type => it is alredy parsed by the parser
       if(! element.type){
+        // if it does not have a type we have to separate it and parse it when we get a semicolon 
         let tempSelector = ""
         for (let char = 0; char < trimedSelector.length; char++) {
           if (trimedSelector[char] == ";") {
             let newAtRule = {selector: tempSelector + ";", type : "udn", styles: tempSelector + ";"}
             extractedAtusedCss.push(newAtRule)
+            usedCss.push(newAtRule)
             tempSelector = ""
           }else{
             tempSelector += trimedSelector[char]
@@ -33,6 +37,7 @@ const {trimSelector} = require("./utils/TrimString");
                 let = newRule = {selector: tempSelector, rules: element.rules}
                 // check if this rule exists inside html add it used else add it to unused
                 extractedusedCss.push(newRule)
+                usedCss.push(newRule)
               }
             }
           }
@@ -40,20 +45,26 @@ const {trimSelector} = require("./utils/TrimString");
         }
       else{
         normalCss.push(element)
+        usedCss.push(element)
       }
     }else if(trimedSelector.startsWith(':')){
       usedATCss.push(element)
+      usedCss.push(element)
     }else{
-      normalCss.push(element)
+      // check for psuedo selectors and remove them
+      if(trimedSelector.includes(":")){
+        trimedSelector = removePsudoSelectors(trimedSelector)
+      }
+      // check if the selector is used using query selector
+      if (parsedHtml.window.document.querySelector(trimedSelector)) {
+        usedCss.push(element)
+      } else {
+        notUsedCss.push(element)
+      }
     }
   });
-  console.log("extracted used css")
-  console.log(extractedusedCss)
-  console.log("extracted at used css")
-  console.log(extractedAtusedCss)
-  console.log("normal used css")
-  console.log(normalCss)
-  console.log("normal at used css")
-  console.log(usedATCss)
-
+  console.log("used css")
+  console.log(usedCss)
+  console.log("not used css")
+  console.log(notUsedCss)
 })();
--- utils/TrimString.js
+++ utils/TrimString.js
@@ -2,3 +2,41 @@ exports.trimSelector = (selector) => {
     word = selector.replace(/\n/g, "");
     return word
 }
+
+exports.removePsudoSelectors = (selector) => {
+    // const ignoredPseudos = [
+    //     /* link */
+    //     ':link',
+    //     ':visited',
+    //     /* user action */
+    //     ':hover',
+    //     ':active',
+    //     ':focus',
+    //     ':focus-within',
+    //     /* UI element states */
+    //     ':enabled',
+    //     ':disabled',
+    //     ':checked',
+    //     ':indeterminate',
+    //     /* form validation */
+    //     ':required',
+    //     ':invalid',
+    //     ':valid',
+    //     /* pseudo elements */
+    //     '::first-line',
+    //     '::first-letter',
+    //     '::selection',
+    //     '::before',
+    //     '::after',
+    //     /* pseudo classes */
+    //     ':target',
+    //     /* CSS2 pseudo elements */
+    //     ':before',
+    //     ':after',
+    //     '::?-(?:moz|ms|webkit|o)-[a-z0-9-]+',
+    // ]
+    // let joined = ignoredPseudos.join('|')
+
+    // return selector.replace(/joined/g, "")
+    return selector.replace(/::?[^ ,:.#]+/g, '');
+}
